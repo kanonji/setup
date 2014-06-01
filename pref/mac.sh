707e22d300a8d5cdad05b00fda29c4680a1cec38
kanonji <kanonji@users.noreply.github.com>
2014-06-02 00:12:46 +0900

    Disable warning on changing extension

--- pref/mac.sh
+++ pref/mac.sh
@@ -1,2 +1,4 @@
 # Disable Dashboard
 defaults write com.apple.dashboard mcx-disabled -boolean true
+# Disable warning on changing extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
