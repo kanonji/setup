aad6b59ceab55bffbdd3d57d6ffd794457e20d6b
kanonji <kanonji@users.noreply.github.com>
2018-12-31 23:00:58 +0900

    Add install tmux plugins

--- main.sh
+++ main.sh
@@ -8,6 +8,7 @@ source ./parts/command_required.sh
 source ./parts/dotfile.sh
 source ./parts/homedir.sh
 source ./parts/vim_plugins.sh
+source ./parts/tmux_plugins.sh
 
 if this_os_is mac; then
     source ./settings/mac.sh
---
+++ parts/tmux_plugins.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo '-------------------'
+echo 'Install tmux plugins.'
+echo '-------------------'
+echo ''
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo '---------------------------'
+echo 'Install tmux plugins...Done.'
+echo '---------------------------'
+echo ''
