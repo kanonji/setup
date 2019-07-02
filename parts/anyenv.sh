8ec2c7eaf8bf70d5de1a0bc3608e7540c705f226
kanonji <kanonji@users.noreply.github.com>
2019-07-02 19:07:26 +0900

    Add installing nodenv-yarn-install

--- parts/anyenv.sh
+++ parts/anyenv.sh
@@ -16,6 +16,7 @@ source ~/dotfile/.bashrc
 anyenv install phpenv
 git clone https://github.com/ngyuki/phpenv-composer.git $(anyenv root)/envs/phpenv/plugins
 anyenv install nodenv
+git clone https://github.com/pine/nodenv-yarn-install.git $(nodenv root)/plugins/nodenv-yarn-install
 anyenv install rbenv
 anyenv install pyenv
 anyenv install goenv
