54b4639ea0504f7bcdf5d140bc0defdcae406feb
kanonji <kanonji@users.noreply.github.com>
2019-07-02 21:10:37 +0900

    Force set timezone if wsl

--- settings/wsl.sh
+++ settings/wsl.sh
@@ -9,3 +9,6 @@ ln -s /mnt/c/Users/$USER/AppData/Roaming/Unity/Asset\ Store-5.x ~/unityAsset
 if [ ! -f /etc/wsl.conf ]; then
   ln -s ~/dotfile/wsl.conf /etc/wsl.conf
 fi
+
+sudo echo Asia/Tokyo > /etc/timezone
+sudo dpkg-reconfigure --frontend noninteractive tzdata
