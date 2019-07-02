f7853a3d874dedcc054fd15987b2cecabd476c74
kanonji <kanonji@users.noreply.github.com>
2019-07-02 20:54:12 +0900

    ln -s wsl.conf if not exist

--- settings/wsl.sh
+++ settings/wsl.sh
@@ -6,3 +6,6 @@ echo ''
 
 ln -s /mnt/c/Users/$USER/dev ~/dev
 ln -s /mnt/c/Users/$USER/AppData/Roaming/Unity/Asset\ Store-5.x ~/unityAsset
+if [ ! -f /etc/wsl.conf ]; then
+  ln -s ~/dotfile/wsl.conf /etc/wsl.conf
+fi
